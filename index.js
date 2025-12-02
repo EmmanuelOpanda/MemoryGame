6335622782f205c3dd65e5df73662df9cd8198d5
Emmanuel Opanda <manueloldboy@gmail.com>
2025-12-02 11:22:19 +0100

    add update for beautification

--- index.js
+++ index.js
@@ -77,11 +77,11 @@ function onCardClick(idx) {
 
     if (flippedCards.length === 1) return;
 
-    // this excecuted only if 2 cards flipeed
     if (gameState[flippedCards[0]].image === gameState[flippedCards[1]].image) {
         gameState[flippedCards[0]].isFlipped = true;
         gameState[flippedCards[1]].isFlipped = true;
         flippedCards = [];
+        checkForWin();
     } else {
         setTimeout(() => {
             document.querySelector(`.card-${flippedCards[0]}`).removeAttribute('src');;
@@ -92,6 +92,14 @@ function onCardClick(idx) {
 
 
 }
+
+function checkForWin() {
+    const allFlipped = gameState.every(card => card.isFlipped === true);
+    if (allFlipped) {
+        document.getElementById("win-popup").classList.remove("hidden");
+    }
+}
+
 
 const onReset = () => {
 
@@ -109,98 +117,10 @@ const onReset = () => {
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let isGameOver = false;
-
-let player = 1;
-
-
- let board = [
-   null, null, null,
-    null, null, null,
-    null, null, null
-]; 
-
-let isWinner = false;
-
-const myModal = document.querySelector('dialog')
-
-const checkWin = () => {
-    if (
-        board[0] === board[1] && board[1] === board[2] && board[0] !== null ||
-        board[3] === board[4] && board[4] === board[5] && board[3] !== null ||
-        board[6] === board[7] && board[7] === board[8] && board[6] !== null ||
-
-        board[0] === board[3] && board[3] === board[6] && board[0] !== null ||
-        board[1] === board[4] && board[4] === board[7] && board[1] !== null ||
-        board[2] === board[5] && board[5] === board[8] && board[2] !== null ||
-
-        board[0] === board[4] && board[4] === board[8] && board[0] !== null ||
-        board[2] === board[4] && board[4] === board[6] && board[2] !== null
-    ) {
-        return true
-    };
-    return false;
+function closeWinPopup() {
+    document.getElementById("win-popup").classList.add("hidden");
+    onReset();
 }
 
-const onCellClick = (idx) => {
-    if (board[idx] !== null) return;
-    if (isGameOver) return;
 
-    if (player === 1) board[idx] = 'X'
-    else {
-        board[idx] = 'O';
 
-    }
-
-    document.querySelector(`.c${idx}`).innerHTML = board[idx];
-
-    if (checkWin()) {
-        isGameOver = true;
-        document.querySelectorAll('.square').forEach(element => {
-            console.log(element)
-            element.classList.add('animate__animated', 'animate__rotateIn');
-        });
-        document.querySelector('.winner').innerHTML = player;
-
-        myModal.show()
-    }
-
-    if (player === 1) player = 2;
-    else player = 1;
-
-    document.querySelector(".player-ph").innerHTML = player;
-
-}
-
-*/
